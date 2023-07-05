8a75bbdf71ffc7578a0f7ee9b5f09ecc1862bd6f
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-07-05 18:50:11 +0300

    add headers to response, add cw, allow Http status 404

--- KYC.API.Proxy/LambdaFunction.cs
+++ KYC.API.Proxy/LambdaFunction.cs
@@ -1,4 +1,5 @@
 using Flurl.Http;
+using Newtonsoft.Json;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json.Linq;
 using Amazon.Lambda.APIGatewayEvents;
@@ -13,7 +14,11 @@ public class LambdaFunction
 
     public async Task<APIGatewayProxyResponse> RunAsync(APIGatewayProxyRequest request)
     {
+        Console.WriteLine(JsonConvert.SerializeObject(request));
+
         var referer = request.Headers["Referer"];
+
+        Console.WriteLine("Referer: " + referer);
 
         var address = request.QueryStringParameters["address"]
             ?? throw new InvalidOperationException("Query string parameters not contain 'address' parameter.");
@@ -27,6 +32,7 @@ public class LambdaFunction
         }
 
         var content = await $"https://kyc.blockpass.org/kyc/1.0/connect/{LambdaSettings.ClientId}/refId/{address}"
+            .AllowHttpStatus("404")
             .WithHeader("Authorization", Settings.SecretApiKey)
             .WithHeader("cache-control", "no-cache")
             .GetAsync()
@@ -34,8 +40,16 @@ public class LambdaFunction
 
         return new APIGatewayProxyResponse
         {
+            IsBase64Encoded = false,
             StatusCode = 200,
-            Body = content.ToString()
+            Body = content.ToString(),
+            Headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/json" },
+                { "Access-Control-Allow-Headers", "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token" },
+                { "Access-Control-Allow-Methods", "GET" },
+                { "Access-Control-Allow-Origin", LambdaSettings.DomainName }
+            }
         };
     }
 }
