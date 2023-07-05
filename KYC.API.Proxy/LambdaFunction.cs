403006480bf394b21369186a933acb882e645a9e
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-07-05 16:07:52 +0300

    initail commit

---
+++ KYC.API.Proxy/LambdaFunction.cs
@@ -0,0 +1,42 @@
+using Flurl.Http;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json.Linq;
+using Amazon.Lambda.APIGatewayEvents;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace KYC.API.Proxy;
+
+public class LambdaFunction
+{
+    public LambdaSettings Settings { get; set; } = new();
+
+    public async Task<APIGatewayProxyResponse> RunAsync(APIGatewayProxyRequest request)
+    {
+        var referer = request.Headers["Referer"];
+        var hashHeader = request.Headers[LambdaSettings.HashHeader];
+
+        if (!referer.Contains(LambdaSettings.DomainName) || hashHeader != LambdaSettings.HashHeader)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 403
+            };
+        }
+
+        var address = request.QueryStringParameters["address"]
+            ?? throw new InvalidOperationException("Query string parameters not contain 'address' parameter.");
+
+        var content = await $"https://kyc.blockpass.org/kyc/1.0/connect/{LambdaSettings.ClientId}/refId/{address}"
+            .WithHeader("Authorization", Settings.SecretApiKey)
+            .WithHeader("cache-control", "no-cache")
+            .GetAsync()
+            .ReceiveJson<JToken>();
+
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = content.ToString()
+        };
+    }
+}
---
+++ KYC.API.Proxy/Readme.md
@@ -0,0 +1,2 @@
+﻿# KYC.API.Proxy
+
