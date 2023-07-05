074fe7015610913e7d86d6bb2faa819c23c4687d
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-07-05 17:02:25 +0300

    remove sumcheck

--- KYC.API.Proxy/LambdaFunction.cs
+++ KYC.API.Proxy/LambdaFunction.cs
@@ -1,8 +1,6 @@
 using Flurl.Http;
-using System.Text;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json.Linq;
-using System.Security.Cryptography;
 using Amazon.Lambda.APIGatewayEvents;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -16,13 +14,11 @@ public class LambdaFunction
     public async Task<APIGatewayProxyResponse> RunAsync(APIGatewayProxyRequest request)
     {
         var referer = request.Headers["Referer"];
-        var hashHeader = request.Headers[LambdaSettings.HashHeader];
 
         var address = request.QueryStringParameters["address"]
             ?? throw new InvalidOperationException("Query string parameters not contain 'address' parameter.");
-        var hashValue = SHA256.HashData(Encoding.UTF8.GetBytes($"{address}{Settings.ExpectedHashValue}"));
 
-        if (!referer.Contains(LambdaSettings.DomainName) || hashHeader != Convert.ToBase64String(hashValue))
+        if (!referer.Contains(LambdaSettings.DomainName))
         {
             return new APIGatewayProxyResponse
             {
--- KYC.API.Proxy/LambdaSettings.cs
+++ KYC.API.Proxy/LambdaSettings.cs
@@ -7,14 +7,11 @@ public class LambdaSettings
 {
     private readonly SecretManager SecretManager;
     private readonly string SecretId = EnvManager.GetEnvironmentValue<string>("SECRET_ID");
-    private readonly string SecretHashKey = EnvManager.GetEnvironmentValue<string>("SECRET_HASH_KEY");
     private readonly string SecretApiKeyName = EnvManager.GetEnvironmentValue<string>("SECRET_API_KEY");
 
     public static string DomainName => EnvManager.GetEnvironmentValue<string>("DOMAIN_NAME");
-    public static string HashHeader => EnvManager.GetEnvironmentValue<string>("HASH_HEADER");
     public static string ClientId => EnvManager.GetEnvironmentValue<string>("CLIENT_ID");
 
-    public string ExpectedHashValue => SecretManager.GetSecretValue(SecretId, SecretHashKey);
     public string SecretApiKey => SecretManager.GetSecretValue(SecretId, SecretApiKeyName);
 
     public LambdaSettings(SecretManager? secretManager = null)
