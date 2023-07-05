70ea324f7666874a1cd01b1ffd76a75a6acb61f9
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-07-05 16:29:30 +0300

    update code logic

--- KYC.API.Proxy/LambdaFunction.cs
+++ KYC.API.Proxy/LambdaFunction.cs
@@ -1,6 +1,8 @@
 using Flurl.Http;
+using System.Text;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json.Linq;
+using System.Security.Cryptography;
 using Amazon.Lambda.APIGatewayEvents;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -16,16 +18,17 @@ public class LambdaFunction
         var referer = request.Headers["Referer"];
         var hashHeader = request.Headers[LambdaSettings.HashHeader];
 
-        if (!referer.Contains(LambdaSettings.DomainName) || hashHeader != Settings.ExpectedHashValue)
+        var address = request.QueryStringParameters["address"]
+            ?? throw new InvalidOperationException("Query string parameters not contain 'address' parameter.");
+        var hashValue = SHA256.HashData(Encoding.UTF8.GetBytes($"{address}{Settings.ExpectedHashValue}"));
+
+        if (!referer.Contains(LambdaSettings.DomainName) || hashHeader != Convert.ToBase64String(hashValue))
         {
             return new APIGatewayProxyResponse
             {
                 StatusCode = 403
             };
         }
-
-        var address = request.QueryStringParameters["address"]
-            ?? throw new InvalidOperationException("Query string parameters not contain 'address' parameter.");
 
         var content = await $"https://kyc.blockpass.org/kyc/1.0/connect/{LambdaSettings.ClientId}/refId/{address}"
             .WithHeader("Authorization", Settings.SecretApiKey)
