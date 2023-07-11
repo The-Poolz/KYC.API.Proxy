5ef529c89092975fcfaaf4b740bd042ca036757c
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-07-11 12:26:29 +0300

    update code

--- KYC.API.Proxy/LambdaFunction.cs
+++ KYC.API.Proxy/LambdaFunction.cs
@@ -1,6 +1,9 @@
+using Amazon;
 using Flurl.Http;
+using Amazon.Lambda;
 using Newtonsoft.Json;
 using Amazon.Lambda.Core;
+using Amazon.Lambda.Model;
 using Newtonsoft.Json.Linq;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -15,7 +18,9 @@ public class LambdaFunction
     {
         Console.WriteLine(JsonConvert.SerializeObject(request));
 
-        var content = await $"https://kyc.blockpass.org/kyc/1.0/connect/{LambdaSettings.ClientId}/refId/{"GET_ADDRESS_FROM_TOKEN_HERE"}"
+        var userAddress = await CallSqlQueryExecutorLambdaAsync("PASTE_USER_TOKEN_HERE");
+
+        var content = await $"https://kyc.blockpass.org/kyc/1.0/connect/{LambdaSettings.ClientId}/refId/{userAddress}"
             .AllowHttpStatus("404")
             .WithHeader("Authorization", Settings.SecretApiKey)
             .WithHeader("cache-control", "no-cache")
@@ -23,5 +28,34 @@ public class LambdaFunction
             .ReceiveJson<JToken>();
 
         return content;
+    }
+
+    public async Task<string> CallSqlQueryExecutorLambdaAsync(string userToken)
+    {
+        var lambdaClient = new AmazonLambdaClient(RegionEndpoint.USEast1);
+
+        var lambdaRequest = new JObject
+        {
+            { "DbName", "AuthDB" },
+            { "Body", $"SELECT TOP(1) [Wallet] FROM [dbo].[AuthorizationTokens] WHERE UserToken = '{userToken}'" }
+        };
+
+        var request = new InvokeRequest
+        {
+            FunctionName = "SqlQueryExecutor",
+            InvocationType = InvocationType.RequestResponse,
+            LogType = LogType.None,
+            Payload = $"{lambdaRequest}"
+        };
+
+        var response = await lambdaClient.InvokeAsync(request);
+
+        if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+        {
+            using var sr = new StreamReader(response.Payload);
+            return await sr.ReadToEndAsync();
+        }
+
+        throw new InvalidOperationException($"Failed to invoke function: {response.StatusCode}");
     }
 }
