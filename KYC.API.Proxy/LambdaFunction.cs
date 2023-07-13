1f82a27d1a54725c733905c731da22ec955d6435
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-07-13 11:54:05 +0300

    update logic, remove lambda call via AWS

--- KYC.API.Proxy/LambdaFunction.cs
+++ KYC.API.Proxy/LambdaFunction.cs
@@ -14,13 +14,17 @@ public class LambdaFunction
 {
     private readonly LambdaSettings Settings = new();
 
-    public async Task<JToken> RunAsync(JToken request)
+    public async Task<JToken> RunAsync(JObject request)
     {
-        Console.WriteLine(JsonConvert.SerializeObject(request));
+        if (!request.ContainsKey("Address"))
+        {
+            return new JObject()
+            {
+                { "StatusCode", 403 }
+            };
+        }
 
-        var userAddress = await CallSqlQueryExecutorLambdaAsync("PASTE_USER_TOKEN_HERE");
-
-        var content = await $"https://kyc.blockpass.org/kyc/1.0/connect/{LambdaSettings.ClientId}/refId/{userAddress}"
+        var content = await $"https://kyc.blockpass.org/kyc/1.0/connect/{LambdaSettings.ClientId}/refId/{request["Address"]}"
             .AllowHttpStatus("404")
             .WithHeader("Authorization", Settings.SecretApiKey)
             .WithHeader("cache-control", "no-cache")
@@ -28,34 +32,5 @@ public class LambdaFunction
             .ReceiveJson<JToken>();
 
         return content;
-    }
-
-    public async Task<string> CallSqlQueryExecutorLambdaAsync(string userToken)
-    {
-        var lambdaClient = new AmazonLambdaClient(RegionEndpoint.USEast1);
-
-        var lambdaRequest = new JObject
-        {
-            { "DbName", "AuthDB" },
-            { "Body", $"SELECT TOP(1) [Wallet] FROM [dbo].[AuthorizationTokens] WHERE UserToken = '{userToken}'" }
-        };
-
-        var request = new InvokeRequest
-        {
-            FunctionName = "SqlQueryExecutor",
-            InvocationType = InvocationType.RequestResponse,
-            LogType = LogType.None,
-            Payload = $"{lambdaRequest}"
-        };
-
-        var response = await lambdaClient.InvokeAsync(request);
-
-        if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
-        {
-            using var sr = new StreamReader(response.Payload);
-            return await sr.ReadToEndAsync();
-        }
-
-        throw new InvalidOperationException($"Failed to invoke function: {response.StatusCode}");
     }
 }
