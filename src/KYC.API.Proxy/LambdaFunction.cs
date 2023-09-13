a883d80260fe2d25b2ada0ddfbd50476f9498d13
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-09-13 16:52:58 +0300

    Add DynamoDb call

--- src/KYC.API.Proxy/LambdaFunction.cs
+++ src/KYC.API.Proxy/LambdaFunction.cs
@@ -1,5 +1,6 @@
 using Flurl.Http;
 using Amazon.Lambda.Core;
+using KYC.API.Proxy.Utils;
 using Newtonsoft.Json.Linq;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -9,26 +10,37 @@ namespace KYC.API.Proxy;
 public class LambdaFunction
 {
     private readonly LambdaSettings settings;
+    private readonly DynamoDb dynamoDb;
 
     public LambdaFunction()
-        : this(new LambdaSettings())
+        : this(new LambdaSettings(), new DynamoDb())
     { }
-    public LambdaFunction(LambdaSettings settings)
+
+    public LambdaFunction(LambdaSettings settings, DynamoDb dynamoDb)
     {
         this.settings = settings;
+        this.dynamoDb = dynamoDb;
     }
 
     public async Task<JToken> RunAsync(JObject request)
     {
-        if (!request.ContainsKey("Address") || request["Address"]?.ToString() == "0x0000000000000000000000000000000000000000")
+        if (!request.ContainsKey("Address") || request["Address"]!.ToString() == "0x0000000000000000000000000000000000000000")
         {
-            return new JObject()
+            return new JObject
             {
                 { "StatusCode", 403 }
             };
         }
 
-        return await $"https://kyc.blockpass.org/kyc/1.0/connect/{LambdaSettings.ClientId}/refId/{request["Address"]}"
+        var address = request["Address"]!.ToString();
+
+        var wallets = await dynamoDb.GetWalletsAsync(address);
+        foreach (var wallet in wallets)
+        {
+            Console.WriteLine(wallet);
+        }
+
+        return await $"https://kyc.blockpass.org/kyc/1.0/connect/{LambdaSettings.ClientId}/refId/{address}"
             .AllowHttpStatus("404")
             .WithHeader("Authorization", settings.SecretApiKey)
             .WithHeader("cache-control", "no-cache")
---
+++ src/KYC.API.Proxy/Utils/DynamoDb.cs
@@ -0,0 +1,47 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+
+namespace KYC.API.Proxy.Utils;
+
+public class DynamoDb
+{
+    private readonly IAmazonDynamoDB client;
+
+    public DynamoDb()
+        : this(new AmazonDynamoDBClient())
+    { }
+
+    public DynamoDb(IAmazonDynamoDB client)
+    {
+        this.client = client;
+    }
+
+    public async Task<string[]> GetWalletsAsync(string wallet)
+    {
+        var request = new QueryRequest
+        {
+            TableName = "UserData",
+            KeyConditionExpression = "EvmWallet = :v_key",
+            ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+            {
+                { ":v_key", new AttributeValue { S = wallet } }
+            }
+        };
+
+        var response = await client.QueryAsync(request);
+
+        if (response.Items.Count == 0)
+        {
+            return Array.Empty<string>();
+        }
+
+        var evmWallets = response.Items[0]["EvmWallets"].L.Select(x => x.S).ToList();
+        var evmWallet = response.Items[0]["EvmWallet"].S;
+
+        return wallet == evmWallet ? evmWallets.ToArray() : 
+            new List<string>(evmWallets)
+            {
+                evmWallet
+            }.ToArray();
+    }
+}
