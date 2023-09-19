a436ec588d63142f98aa46976fbfcf6b4fe213e9
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-09-19 14:13:40 +0300

    add one more test

--- tests/KYC.API.Proxy.Tests/Utils/DynamoDbTests.cs
+++ tests/KYC.API.Proxy.Tests/Utils/DynamoDbTests.cs
@@ -1,9 +1,9 @@
 ﻿using Moq;
 using Xunit;
 using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.Model;
 using FluentAssertions;
 using KYC.API.Proxy.Utils;
+using Amazon.DynamoDBv2.Model;
 
 namespace KYC.API.Proxy.Tests.Utils;
 
@@ -15,6 +15,25 @@ public class DynamoDbTests
         var client = new Mock<IAmazonDynamoDB>();
         client.Setup(x => x.GetItemAsync(It.IsAny<GetItemRequest>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync(new GetItemResponse());
+        var dynamoDb = new DynamoDb(client.Object);
+
+        var result = dynamoDb.GetWallets("wallet");
+
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    internal void GetWallets_EvmWalletsNotFoundInUser()
+    {
+        var client = new Mock<IAmazonDynamoDB>();
+        client.Setup(x => x.GetItemAsync(It.IsAny<GetItemRequest>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new GetItemResponse
+            {
+                Item = new Dictionary<string, AttributeValue>
+                {
+                    { "EvmWallet", new AttributeValue() }
+                }
+            });
         var dynamoDb = new DynamoDb(client.Object);
 
         var result = dynamoDb.GetWallets("wallet");
