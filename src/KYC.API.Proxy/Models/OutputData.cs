62919a8416bf936a95f9abdbc3e7bf0b28358ecb
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-09-20 11:03:18 +0300

    add models

---
+++ src/KYC.API.Proxy/Models/OutputData.cs
@@ -0,0 +1,8 @@
+﻿namespace KYC.API.Proxy.Models;
+
+public class OutputData
+{
+    public string RequestStatus { get; set; } = null!;
+    public string? Status { get; set; }
+    public string? Name { get; set; }
+}
