91eacc715f329ea1f76a0e39f1fd164747353995
Stan Goldin <salty87@gmail.com>
2023-09-27 12:55:55 +0300

    issue_#28

--- src/KYC.API.Proxy/Models/HttpResponse/GivenName.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace KYC.API.Proxy.Models.HttpResponse;
-
-public class GivenName
-{
-    [JsonProperty("value")]
-    public string Value { get; set; } = null!;
-}
--- src/KYC.API.Proxy/Models/HttpResponse/Identities.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace KYC.API.Proxy.Models.HttpResponse;
-
-public class Identities
-{
-    [JsonProperty("given_name")]
-    public GivenName GivenName { get; set; } = new();
-}
