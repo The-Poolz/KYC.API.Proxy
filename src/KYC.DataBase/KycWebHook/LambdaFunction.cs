2654d908ce011f976d863a8544369b355dd3fd0a
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-10-10 15:36:45 +0300

    - install KYC.DataBase package - add input model for lambda - cw input

--- src/KYC.DataBase/KycWebHook/LambdaFunction.cs
+++ src/KYC.DataBase/KycWebHook/LambdaFunction.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using Amazon.Lambda.Core;
+using KycWebHook.Models;
 using Newtonsoft.Json.Linq;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -8,9 +9,11 @@ namespace KycWebHook;
 
 public class LambdaFunction
 {
-    public int Run(JObject input)
+    public int Run(HttpResponse httpResponse)
     {
         // save into db
+
+        Console.WriteLine(JToken.FromObject(httpResponse));
 
         return (int)HttpStatusCode.OK;
     }
