7864f63ae9cc8fb0cdd57abfe59a0910bca0c529
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-10-10 15:43:21 +0300

    update lambda function

--- src/KYC.DataBase/KycWebHook/LambdaFunction.cs
+++ src/KYC.DataBase/KycWebHook/LambdaFunction.cs
@@ -1,6 +1,7 @@
 using System.Net;
-using Amazon.Lambda.Core;
+using KYC.DataBase;
 using KycWebHook.Models;
+using Amazon.Lambda.Core;
 using Newtonsoft.Json.Linq;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -9,11 +10,31 @@ namespace KycWebHook;
 
 public class LambdaFunction
 {
-    public int Run(HttpResponse httpResponse)
-    {
-        // save into db
+    private readonly KycDbContext context;
 
+    public LambdaFunction()
+        : this(new KycDbContext())
+    { }
+
+    public LambdaFunction(KycDbContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<int> Run(HttpResponse httpResponse)
+    {
         Console.WriteLine(JToken.FromObject(httpResponse));
+
+        var user = context.Users.FirstOrDefault(x => x.RecordId == httpResponse.RecordId);
+        if (user == null)
+        {
+            context.Users.Add(httpResponse);
+        }
+        else
+        {
+            context.Users.Update(httpResponse);
+        }
+        await context.SaveChangesAsync();
 
         return (int)HttpStatusCode.OK;
     }
