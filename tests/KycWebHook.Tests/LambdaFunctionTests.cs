4e7e09fcb31529765547bebe71ad3e7008faebb1
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2023-10-11 14:10:16 +0300

    Create `KycWebHook` lambda (#34)
    * Add `KycWebHook` lambda
    
    * - install KYC.DataBase package
    - add input model for lambda
    - cw input
    
    * update lambda function
    
    * add tests
    
    * fix test, cleanup tests
    
    * Create nuget.config (#38)

---
+++ tests/KycWebHook.Tests/LambdaFunctionTests.cs
@@ -0,0 +1,45 @@
+using ConfiguredSqlConnection.Extensions;
+using Xunit;
+using FluentAssertions;
+using KYC.DataBase;
+using KycWebHook.Models;
+using System.Net;
+
+namespace KycWebHook.Tests;
+
+public class LambdaFunctionTests
+{
+    private readonly HttpResponse httpResponse = new()
+    {
+        Guid = "614967cde3227d00125ebc4f",
+        Status = "deleted",
+        ClientId = "client_id",
+        Event = "user.deleted",
+        RecordId = "5ffffb44baaaaf001236b1d1",
+        RefId = null
+    };
+
+    [Fact]
+    public async Task RunAsync_WhenUserNotExist()
+    {
+        var context = new DbContextFactory<KycDbContext>().Create(ContextOption.InMemory, Guid.NewGuid().ToString());
+
+        var result = await new LambdaFunction(context).RunAsync(httpResponse);
+
+        result.Should().Be((int)HttpStatusCode.OK);
+        context.Users.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public async Task RunAsync_WhenUserExist()
+    {
+        var context = new DbContextFactory<KycDbContext>().Create(ContextOption.InMemory, Guid.NewGuid().ToString());
+        context.Users.Add(httpResponse);
+        await context.SaveChangesAsync();
+
+        var result = await new LambdaFunction(context).RunAsync(httpResponse);
+
+        result.Should().Be((int)HttpStatusCode.OK);
+        context.Users.First().Should().BeEquivalentTo(httpResponse);
+    }
+}
