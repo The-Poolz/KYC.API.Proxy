9c3d62a9b72b2babcaa4b12da906f7bbb75d22dd
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-10-11 12:00:42 +0300

    fix test, cleanup tests

--- tests/KycWebHook.Tests/LambdaFunctionTests.cs
+++ tests/KycWebHook.Tests/LambdaFunctionTests.cs
@@ -3,7 +3,6 @@ using Xunit;
 using FluentAssertions;
 using KYC.DataBase;
 using KycWebHook.Models;
-using Moq;
 using System.Net;
 
 namespace KycWebHook.Tests;
@@ -18,7 +17,6 @@ public class LambdaFunctionTests
         Event = "user.deleted",
         RecordId = "5ffffb44baaaaf001236b1d1",
         RefId = null
-        
     };
 
     [Fact]
@@ -35,22 +33,13 @@ public class LambdaFunctionTests
     [Fact]
     public async Task RunAsync_WhenUserExist()
     {
-        var updatedUser = new HttpResponse
-        {
-            Guid = "614967cde3227d00125ebc4f",
-            Status = "blocked",
-            ClientId = "client_id",
-            Event = "user.deleted",
-            RecordId = "5ffffb44baaaaf001236b1d1",
-            RefId = null
-        };
         var context = new DbContextFactory<KycDbContext>().Create(ContextOption.InMemory, Guid.NewGuid().ToString());
         context.Users.Add(httpResponse);
         await context.SaveChangesAsync();
 
-        var result = await new LambdaFunction(context).RunAsync(updatedUser);
+        var result = await new LambdaFunction(context).RunAsync(httpResponse);
 
         result.Should().Be((int)HttpStatusCode.OK);
-        context.Users.First().Should().BeEquivalentTo(updatedUser);
+        context.Users.First().Should().BeEquivalentTo(httpResponse);
     }
 }
