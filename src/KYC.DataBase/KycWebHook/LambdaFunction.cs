8fb7a4a63a4484ca09b059197312011ec2afbd32
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-10-12 14:02:55 +0300

    update LambdaFunction

--- src/KYC.DataBase/KycWebHook/LambdaFunction.cs
+++ src/KYC.DataBase/KycWebHook/LambdaFunction.cs
@@ -1,8 +1,12 @@
 using System.Net;
+using System.Security.Cryptography;
 using KYC.DataBase;
+using Newtonsoft.Json;
 using KycWebHook.Models;
 using Amazon.Lambda.Core;
-using Newtonsoft.Json.Linq;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Text;
+using System.Text.RegularExpressions;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -21,9 +25,30 @@ public class LambdaFunction
         this.context = context;
     }
 
-    public async Task<int> RunAsync(HttpResponse httpResponse)
+    public async Task<APIGatewayProxyResponse> RunAsync(APIGatewayProxyRequest request)
     {
-        Console.WriteLine(JToken.FromObject(httpResponse));
+        Console.WriteLine(JsonConvert.SerializeObject(request));
+
+        if (!request.Headers.ContainsKey("X-Hub-Signature"))
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.BadRequest
+            };
+        }
+
+        var actualSignature = StringToSha256(Regex.Unescape(request.Body));
+        var signature = request.Headers["X-Hub-Signature"];
+
+        if (actualSignature != signature)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.BadRequest
+            };
+        }
+
+        var httpResponse = JsonConvert.DeserializeObject<HttpResponse>(request.Body)!;
 
         var user = context.Users.FirstOrDefault(x => x.RecordId == httpResponse.RecordId);
         if (user == null)
@@ -36,6 +61,22 @@ public class LambdaFunction
         }
         await context.SaveChangesAsync();
 
-        return (int)HttpStatusCode.OK;
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.OK
+        };
+    }
+
+    public static string StringToSha256(string str)
+    {
+        using var sha256Hash = SHA256.Create();
+        var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(str));
+
+        var builder = new StringBuilder();
+        foreach (var t in bytes)
+        {
+            builder.Append(t.ToString("x2"));
+        }
+        return builder.ToString();
     }
 }
