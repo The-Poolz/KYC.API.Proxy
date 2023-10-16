1a81a195719ca237bd9322d0e3fd77a1f322533d
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2023-10-16 14:21:25 +0300

    Add check authorization header for `KycWebHook` lambda (#41)
    * update LambdaFunction
    
    * add cw
    
    * update LambdaFunction
    
    * update one test
    
    * fix second test using mock

--- src/KYC.DataBase/KycWebHook/LambdaFunction.cs
+++ src/KYC.DataBase/KycWebHook/LambdaFunction.cs
@@ -1,8 +1,11 @@
-using System.Net;
+using System.Text;
 using KYC.DataBase;
+using Newtonsoft.Json;
 using KycWebHook.Models;
 using Amazon.Lambda.Core;
-using Newtonsoft.Json.Linq;
+using System.Security.Cryptography;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Text.RegularExpressions;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -21,9 +24,27 @@ public class LambdaFunction
         this.context = context;
     }
 
-    public async Task<int> RunAsync(HttpResponse httpResponse)
+    public async Task<APIGatewayProxyResponse> RunAsync(APIGatewayProxyRequest request)
     {
-        Console.WriteLine(JToken.FromObject(httpResponse));
+        Console.WriteLine(JsonConvert.SerializeObject(request));
+
+        if (!request.Headers.ContainsKey("X-Hub-Signature"))
+        {
+            return Responses.MissingSignature;
+        }
+
+        var actualSignature = StringToSha256(Regex.Unescape(request.Body));
+        var signature = request.Headers["X-Hub-Signature"];
+
+        Console.WriteLine($"Actual Signature: {actualSignature}");
+        Console.WriteLine($"Signature: {signature}");
+
+        if (actualSignature != signature)
+        {
+            return Responses.InvalidSignature;
+        }
+
+        var httpResponse = JsonConvert.DeserializeObject<HttpResponse>(request.Body)!;
 
         var user = context.Users.FirstOrDefault(x => x.RecordId == httpResponse.RecordId);
         if (user == null)
@@ -36,6 +57,19 @@ public class LambdaFunction
         }
         await context.SaveChangesAsync();
 
-        return (int)HttpStatusCode.OK;
+        return Responses.OkResponse;
+    }
+
+    public static string StringToSha256(string str)
+    {
+        using var sha256Hash = SHA256.Create();
+        var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(str));
+
+        var builder = new StringBuilder();
+        foreach (var t in bytes)
+        {
+            builder.Append(t.ToString("x2"));
+        }
+        return builder.ToString();
     }
 }
--- tests/KycWebHook.Tests/LambdaFunctionTests.cs
+++ tests/KycWebHook.Tests/LambdaFunctionTests.cs
@@ -1,45 +1,68 @@
-using ConfiguredSqlConnection.Extensions;
 using Xunit;
-using FluentAssertions;
-using KYC.DataBase;
-using KycWebHook.Models;
 using System.Net;
+using KYC.DataBase;
+using Newtonsoft.Json;
+using FluentAssertions;
+using KycWebHook.Models;
+using Amazon.Lambda.APIGatewayEvents;
+using ConfiguredSqlConnection.Extensions;
+using KYC.DataBase.Models;
+using Microsoft.EntityFrameworkCore;
+using TestableDbContext.Mock;
 
 namespace KycWebHook.Tests;
 
 public class LambdaFunctionTests
 {
-    private readonly HttpResponse httpResponse = new()
+    private readonly HttpResponse httpResponse;
+
+    private readonly APIGatewayProxyRequest request;
+
+    public LambdaFunctionTests()
     {
-        Guid = "614967cde3227d00125ebc4f",
-        Status = "deleted",
-        ClientId = "client_id",
-        Event = "user.deleted",
-        RecordId = "5ffffb44baaaaf001236b1d1",
-        RefId = null
-    };
+        httpResponse = new HttpResponse
+        {
+            Guid = "614967cde3227d00125ebc4f",
+            Status = "deleted",
+            ClientId = "client_id",
+            Event = "user.deleted",
+            RecordId = "5ffffb44baaaaf001236b1d1",
+            RefId = null
+        };
+
+        var strHttpResponse = JsonConvert.SerializeObject(httpResponse);
+        request = new APIGatewayProxyRequest
+        {
+            Headers = new Dictionary<string, string>
+            {
+                { "X-Hub-Signature", LambdaFunction.StringToSha256(strHttpResponse) }
+            },
+            Body = strHttpResponse
+        };
+    }
 
     [Fact]
     public async Task RunAsync_WhenUserNotExist()
     {
         var context = new DbContextFactory<KycDbContext>().Create(ContextOption.InMemory, Guid.NewGuid().ToString());
 
-        var result = await new LambdaFunction(context).RunAsync(httpResponse);
+        var result = await new LambdaFunction(context).RunAsync(request);
 
-        result.Should().Be((int)HttpStatusCode.OK);
+        result.Should().BeEquivalentTo(Responses.OkResponse);
         context.Users.Should().HaveCount(1);
     }
 
     [Fact]
     public async Task RunAsync_WhenUserExist()
     {
-        var context = new DbContextFactory<KycDbContext>().Create(ContextOption.InMemory, Guid.NewGuid().ToString());
-        context.Users.Add(httpResponse);
-        await context.SaveChangesAsync();
+        var mockContext = MockDbContext<KycDbContext>.GetMockContext();
+        var users = new List<User> { httpResponse };
+        var mockDbSet = MockDbContext.GetMockDbSet(users);
+        mockContext.Setup(x => x.Users).Returns(mockDbSet.Object);
 
-        var result = await new LambdaFunction(context).RunAsync(httpResponse);
+        var result = await new LambdaFunction(mockContext.Object).RunAsync(request);
 
-        result.Should().Be((int)HttpStatusCode.OK);
-        context.Users.First().Should().BeEquivalentTo(httpResponse);
+        result.Should().BeEquivalentTo(Responses.OkResponse);
+        mockContext.Object.Users.First().Should().BeEquivalentTo(httpResponse);
     }
 }
