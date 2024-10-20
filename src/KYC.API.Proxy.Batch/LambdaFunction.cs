b47fc85c93802ff314f9a27d702e8c605dab5e37
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-10-20 20:31:00 +0300

    Create `KYC.API.Proxy.Batch` lambda

---
+++ src/KYC.API.Proxy.Batch/LambdaFunction.cs
@@ -0,0 +1,43 @@
+using Flurl.Http;
+using Amazon.Lambda.Core;
+using KYC.API.Proxy.Models;
+using KYC.API.Proxy.Batch.Models;
+using Net.Web3.EthereumWallet.Extensions;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace KYC.API.Proxy.Batch;
+
+public class LambdaFunction(Proxy.LambdaFunction kycFunc)
+{
+    public LambdaFunction() : this(new Proxy.LambdaFunction()) { }
+
+    public async Task<IEnumerable<LambdaResponse>> RunAsync(IEnumerable<string> listOfAddress)
+    {
+        var responses = new List<LambdaResponse>();
+
+        foreach (var address in listOfAddress)
+        {
+            var success = false;
+
+            while (!success)
+            {
+                try
+                {
+                    var checksumAddress = address.ConvertToChecksumAddress();
+                    var response = kycFunc.Run(new InputData { Address = checksumAddress });
+
+                    responses.Add(new LambdaResponse(checksumAddress, response));
+                    success = true;
+                }
+                catch (FlurlHttpException)
+                {
+                    Console.WriteLine($"Error processing address: {address}. Retrying...");
+                    await Task.Delay(2500);
+                }
+            }
+        }
+
+        return responses;
+    }
+}
