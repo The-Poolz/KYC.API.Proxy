db2f27b591411637c5d2fb5f5ce790fda7ca7b8b
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2024-10-22 15:20:03 +0300

    Create `KYC.API.Proxy.Batch` lambda (#64)
    * Create `KYC.API.Proxy.Batch` lambda
    
    * - log error message

---
+++ src/KYC.API.Proxy.Batch/Models/LambdaResponse.cs
@@ -0,0 +1,15 @@
+﻿using KYC.API.Proxy.Models;
+
+namespace KYC.API.Proxy.Batch.Models;
+
+public class LambdaResponse(string address, string requestStatus, string? status, string? proxy)
+{
+    public LambdaResponse(string address, OutputData data)
+        : this(address, data.RequestStatus.ToString(), data.Status, data.Proxy)
+    { }
+
+    public string Address { get; } = address;
+    public string RequestStatus { get; } = requestStatus;
+    public string? Status { get; } = status;
+    public string? Proxy { get; } = proxy;
+}
