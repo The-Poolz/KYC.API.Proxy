fe975e555b2cda149ccaed7bcea9bda4ff294f65
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-11-27 13:20:57 +0300

    - just fix exist test - add attributes to mark envs as required

--- src/AdminKycProxy/Env.cs
+++ src/AdminKycProxy/Env.cs
@@ -1,9 +1,15 @@
-﻿namespace AdminKycProxy;
+﻿using EnvironmentManager.Attributes;
+
+namespace AdminKycProxy;
 
 public enum Env
 {
+    [EnvironmentVariable(isRequired: true)]
     SECRET_ID,
+    [EnvironmentVariable(isRequired: true)]
     SECRET_API_KEY,
+    [EnvironmentVariable(isRequired: true)]
     KYC_URL,
+    [EnvironmentVariable(isRequired: true, type: typeof(int))]
     PAGE_SIZE
 }
--- tests/AdminKycProxy.Tests/LambdaFunctionTests.cs
+++ tests/AdminKycProxy.Tests/LambdaFunctionTests.cs
@@ -7,7 +7,8 @@ using FluentAssertions;
 using Flurl.Http.Testing;
 using KYC.DataBase.Models;
 using AdminKycProxy.Models;
-using Amazon.Lambda.TestUtilities;
+using KYC.DataBase.Models.Types;
+using Microsoft.EntityFrameworkCore;
 using ConfiguredSqlConnection.Extensions;
 
 namespace AdminKycProxy.Tests;
@@ -16,7 +17,7 @@ public class LambdaFunctionTests
 {
     public LambdaFunctionTests()
     {
-        Environment.SetEnvironmentVariable("DOWNLOADED_FROM", "0");
+        Environment.SetEnvironmentVariable("PAGE_SIZE", "20");
         Environment.SetEnvironmentVariable("SECRET_ID", "SecretId");
         Environment.SetEnvironmentVariable("SECRET_API_KEY", "SecretApiKey");
         Environment.SetEnvironmentVariable("KYC_URL", "https://kyc.blockpass.org/kyc/1.0/connect/ClientId/applicants");
@@ -45,14 +46,13 @@ public class LambdaFunctionTests
                 Limit = 20,
                 Skip = 0,
                 Total = 1,
-                Records = new[]
-                {
+                Records = [
                     new User
                     {
                         RecordId = Guid.NewGuid().ToString(),
-                        Status = "approved"
+                        Status = Status.approved
                     }
-                }
+                ]
             }
         };
         using var httpTest = new HttpTest();
@@ -63,13 +63,14 @@ public class LambdaFunctionTests
             .ForCallsTo("https://kyc.blockpass.org/kyc/1.0/connect/ClientId/applicants?limit=20&skip=20")
             .RespondWithJson(new HttpResponse());
 
-        var context = new DbContextFactory<KycDbContext>().Create(ContextOption.InMemory, Guid.NewGuid().ToString());
+        var contextFactory = new Mock<IDbContextFactory<KycDbContext>>();
+        contextFactory.Setup(x => x.CreateDbContextAsync(default))
+            .ReturnsAsync(() => new DbContextFactory<KycDbContext>().Create(ContextOption.InMemory, Guid.NewGuid().ToString()));
 
-        var lambda = new AdminKycProxyLambda(secretManager.Object, context);
+        var lambda = new AdminKycProxyLambda(secretManager.Object, contextFactory.Object);
 
-        var result = await lambda.RunAsync(new TestLambdaContext());
+        var result = await lambda.RunAsync();
 
         result.Should().Be(HttpStatusCode.OK);
-        context.Users.Should().HaveCount(1);
     }
 }
